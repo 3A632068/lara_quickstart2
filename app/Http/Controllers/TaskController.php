f96c6701c2769e239bd4de3c489e6e682dedcf77
PengHueiSin <eason12326@gmail.com>
2019-12-18 21:52:11 +0800

    限制三個處理任務路由只讓已認證的使用者存取

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    //限制三個處理任務路由只讓已認證的使用者存取
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 }
