82b93e9fc0484b065a2bfcbb150a63cbecda5a33
PengHueiSin <eason12326@gmail.com>
2019-12-18 21:42:04 +0800

    Eloquent 關聯

--- app/Task.php
+++ app/Task.php
@@ -8,4 +8,9 @@ class Task extends Model
 {
     //Eloquent 模型
     protected $fillable = ['name'];
+    //Eloquent關聯-user關聯
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //Eloquent關聯-tasks關聯
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
